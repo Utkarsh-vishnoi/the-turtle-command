e823bd05a2990c811fcc3630e5acadcb8fdaf787
Utkarsh Vishnoi <utkarshvishnoi25@gmail.com>
2017-05-23 01:34:42 +0530

    Improved UI, Bug Fixes and implemented input & output

--- scripts/turtle.js
+++ scripts/turtle.js
@@ -1,118 +1,196 @@
 $(document).ready(function() {
-	render();
-	$('#grid_size').on("change", function() {
-		render();
-	});
-
-	var direction =  'N';
-	var directionset = 'NESW';
-	function findLeft(alpha) {
-		var pos = directionset.search(alpha);
-		if (pos == 0) 
-			return directionset[directionset.length - 1];
-		else
-			return directionset[pos - 1];
-	}
-	function findRight(alpha) {
-		var pos = directionset.search(alpha);
-		if (pos == directionset.length - 1) 
-			return directionset[0];
-		else
-			return directionset[pos + 1];
-	}
-	$('#movement_left').on("click", function() {
-		direction = findLeft(direction);
-	});
-	$('#movement_top').on("click", function() {
-		
-	});
-	$('#movement_right').on("click", function() {
-		direction = findRight(direction);
-		console.log(direction);
-	});
-});
-function render() {
-	n = $('#grid_size').val();
 	var canvas = document.getElementById('screen');
 	if (!canvas.getContext) {
 		alert("Oh, snap!");
 	}
 	var context = canvas.getContext("2d");
-	context.clearRect(0, 0, canvas.width, canvas.height);
-	if(n < 3) {
-		context.fillStyle = "red";
-		context.fillText("Note: Atleast 3 elements must be present in each side of the grid.", 0, 10);
-	}
+	var direction =  'N';
 	var main = [];
-	var point = function (x, y, type) {
-		return {
-			x: x,
-			y: y,
-			type: type,
-			canvasX: 0,
-			canvasY: 0,
-			obstacle: true
+	var markedPoint, curPos;
+	var directionset = 'NESW';
+	$('#movement_left').on("click", function() {
+		left();
+	});
+	$('#movement_top').on("click", function() {
+		front();
+	});
+
+	function left() {
+		direction = findLeft(direction);
+		output();
+	}
+
+	function front() {
+		switch(direction) {
+			case "N":
+				if (curPos.x < n - 1) {
+					if(!main[curPos.x + 1][curPos.y].obstacle == true)
+						markPoint(context, main[curPos.x + 1][curPos.y]);
+				}
+				break;
+
+			case "S":
+				if (curPos.x - 1 >= 0) {
+					if(!main[curPos.x - 1][curPos.y].obstacle == true)
+						markPoint(context, main[curPos.x - 1][curPos.y]);
+				}
+				break;
+
+			case "E":
+				if (curPos.y < n - 1) {
+					if(!main[curPos.x][curPos.y + 1].obstacle == true)
+						markPoint(context, main[curPos.x][curPos.y + 1]);
+				}
+				break;
+
+			case "W":
+				if (curPos.y - 1 >= 0) {
+					if(!main[curPos.x][curPos.y - 1].obstacle == true)
+						markPoint(context, main[curPos.x][curPos.y - 1]);
+				}
+				break;
+		}
+		output();
+	}
+
+	function right() {
+		direction = findRight(direction);
+		output();
+	}
+
+	$('#movement_right').on("click", function() {
+		right();
+	});
+	function output() {
+		outputs.innerHTML = (curPos.x + 1) + "," + (curPos.y + 1) + " " + direction;
+	}
+	function render() {
+		n = $('#grid_size').val();
+		context.clearRect(0, 0, canvas.width, canvas.height);
+		if(n < 3) {
+			context.fillStyle = "red";
+			context.fillText("Note: Atleast 3 elements must be present in each side of the grid.", 0, 10);
+		}
+		var point = function (x, y, type) {
+			return {
+				x: x,
+				y: y,
+				type: type,
+				canvasX: 0,
+				canvasY: 0,
+				obstacle: false
+			};
 		};
-	};
-	for(var i = 0; i < n; i++) {
-		main[i] = [];
-		for (var j = 0; j < n; j++) {
-			if(i > 0 && i < n-1 && j > 0 && j < n-1) {
-				// Inner point
-				main[i][j] = point(i + 1, j + 1, "I");
+		for(var i = 0; i < n; i++) {
+			main[i] = [];
+			for (var j = 0; j < n; j++) {
+				if(i > 0 && i < n-1 && j > 0 && j < n-1) {
+					// Inner point
+					main[i][j] = point(i, j, "I");
+				}
+				else {
+					// Outer point
+					main[i][j] = point(i, j, "O");
+				}
 			}
-			else {
-				// Outer point
-				main[i][j] = point(i + 1, j + 1, "O");
+		}
+		var x = 20;
+		var y = canvas.height + 30;
+		// Draw circles for grids
+		for(var i = 0; i < n; i++) {
+			y -= 50;
+			x = 20;
+			for (var j = 0; j < n; j++) {
+				drawCircle(context, x, y);
+				main[i][j].canvasX = x;
+				main[i][j].canvasY = y;
+				x+=50;
 			}
 		}
-	}
-	var output = document.getElementById("output");
-	var x = 10;
-	var y = canvas.height + 40;
-	// Draw circles for grids
-	for(var i = 0; i < n; i++) {
-		y -= 50;
-		x = 10;
-		for (var j = 0; j < n; j++) {
-			drawCircle(context, x, y);
-			main[i][j].canvasX = x;
-			main[i][j].canvasY = y;
-			x+=50;
+		// Draw lines to connect circles and form grids
+		for(var i = 0; i < n; i++) {
+			for (var j = 0; j < n; j++) {
+				drawLine(context, main[i][j].canvasX, main[i][j].canvasY, main[i][j].type, i, j);
+			}
 		}
-	}
-	// Draw lines to connect circles and form grids
-	for(var i = 0; i < n; i++) {
-		for (var j = 0; j < n; j++) {
-			drawLine(context, main[i][j].canvasX, main[i][j].canvasY, main[i][j].type);
-		}
-	}
-	// Mark the turtle's starting point
-	markPoint(main[0][0]);
+		// Mark the turtle's starting point
+		markPoint(context, main[0][0]);
 
-	// Draw random n obstacles
-	var obstacles= [];
-	for (var i = 0; i < n; i++) {
-		var coords = getRandomCoords(1, n - 1);
-		var match = searchObstacles(coords, obstacles);
-		if (match) {
-			i--;
+		// Draw random n obstacles
+		var obstacles= [{x:0,y:0}];
+		for (var i = 0; i < n; i++) {
+			var coords = getRandomCoords(0, n - 1);
+			var match = searchObstacles(coords, obstacles);
+			if (match) {
+				i--;
+			}
+			else
+			{
+				obstacles[obstacles.length] = coords;
+				drawObstacles(context, coords.x, coords.y, main);
+			}
 		}
-		obstacles[obstacles.length] = coords;
-		drawObstacles(context, coords.x, coords.y);
+		output();
 	}
 
-	function drawCircle(context, x, y) {
+	function searchObstacles(searchKey, obstacleArray) {
+		for (var i = 0; i < obstacleArray.length; i++) {
+			if(obstacleArray[i].x == searchKey.x && obstacleArray[i].y == searchKey.y)
+				return true;
+		}
+		return false;
+	}
+
+	function drawObstacles(context, i, j, main) {
+		var x = main[i][j].canvasX;
+		var y = main[i][j].canvasY;
+		main[i][j].obstacle = true;
 		context.beginPath();
-		var radius = 8;
-		context.lineWidth = 2;
-		context.strokeStyle = "black";
-		context.arc(x, y, radius, 0, 2*Math.PI);
+		context.moveTo(x, y);
+		context.strokeStyle = "green";
+		context.fillStyle = "green";
+		context.arc(x, y, 6, 0, 2 * Math.PI);
 		context.stroke();
+		context.fill();
 		context.closePath();
 	}
 
-	function drawLine(context, x, y, type) {
+	function getRandomCoords(min, max) {
+		return {
+			x: Math.floor(Math.random() * (max - min + 1)) + min,
+			y: Math.floor(Math.random() * (max - min + 1)) + min
+		};
+	}
+
+	
+	var outputs = document.getElementById("output");
+	function markPoint(context, mainObj) {
+		if(typeof(markedPoint) != "undefined") {
+			context.beginPath();
+			context.moveTo(markedPoint.canvasX, markedPoint.canvasY);
+			context.strokeStyle = "white";
+			context.fillStyle = "white";
+			context.arc(markedPoint.canvasX, markedPoint.canvasY, 6, 0, 2 * Math.PI);
+			context.stroke();
+			context.fill();
+			context.closePath();
+			markedPoint = undefined;
+		}
+		var x = mainObj.canvasX;
+		var y = mainObj.canvasY;
+		context.beginPath();
+		context.moveTo(x, y);
+		context.strokeStyle = "red";
+		context.fillStyle = "red";
+		context.arc(x, y, 6, 0, 2 * Math.PI);
+		context.stroke();
+		context.fill();
+		context.closePath();
+		markedPoint = mainObj;
+		curPos = mainObj;
+	}
+
+	function drawLine(context, x, y, type, i, j) {
 		context.beginPath();
 		context.strokeStyle = "blue";
 		if (type == "O") {
@@ -142,58 +220,56 @@ function render() {
 		context.closePath();
 	}
 
-	var markedPoint;
-	function markPoint(mainObj) {
-		if(typeof(markedPoint) != "undefined") {
-			context.beginPath();
-			context.moveTo(markedPoint.canvasX, markedPoint.canvasY);
-			context.strokeStyle = "white";
-			context.fillStyle = "white";
-			context.arc(markedPoint.canvasX, markedPoint.canvasY, 6, 0, 2 * Math.PI);
-			context.stroke();
-			context.fill();
-			context.closePath();
-			markedPoint = undefined;
-		}
-		var x = mainObj.canvasX;
-		var y = mainObj.canvasY;
+	function drawCircle(context, x, y) {
 		context.beginPath();
-		context.moveTo(x, y);
-		context.strokeStyle = "red";
-		context.fillStyle = "red";
-		context.arc(x, y, 6, 0, 2 * Math.PI);
+		var radius = 8;
+		context.lineWidth = 2;
+		context.strokeStyle = "black";
+		context.arc(x, y, radius, 0, 2*Math.PI);
 		context.stroke();
-		context.fill();
-		context.closePath();
-		markedPoint = mainObj;
-	}
-
-	function getRandomCoords(min, max) {
-		return {
-			x: Math.floor(Math.random() * (max - min + 1)) + min,
-			y: Math.floor(Math.random() * (max - min + 1)) + min
-		};
-	}
-
-	function drawObstacles(context, i, j) {
-		var x = main[i][j].canvasX;
-		var y = main[i][j].canvasY;
-		main[i][j].obstacle = true;
-		context.beginPath();
-		context.moveTo(x, y);
-		context.strokeStyle = "green";
-		context.fillStyle = "green";
-		context.arc(x, y, 6, 0, 2 * Math.PI);
-		context.stroke();
-		context.fill();
 		context.closePath();
 	}
 
-	function searchObstacles(searchKey, obstacleArray) {
-		for (var i = 0; i < obstacleArray.length; i++) {
-			if(obstacleArray[i].x == searchKey.x && obstacleArray[i].y == searchKey.y)
-				return true;
-		}
-		return false;
+	render();
+	$('#grid_size').on("change", function() {
+		render();
+	});
+	
+	function findLeft(alpha) {
+		var pos = directionset.search(alpha);
+		if (pos == 0) 
+			return directionset[directionset.length - 1];
+		else
+			return directionset[pos - 1];
 	}
-}
+	
+	function findRight(alpha) {
+		var pos = directionset.search(alpha);
+		if (pos == directionset.length - 1) 
+			return directionset[0];
+		else
+			return directionset[pos + 1];
+	}
+
+	$("#go").on("click", function() {
+		var inputs = $("#input").val().toUpperCase();
+		var input = [];
+		for (i=0;i<inputs.length;i++) {
+			input[input.length] = inputs[i];
+		}
+		$.each(input, function(index, value) {
+			switch(value) {
+				case "F": front();
+					break;
+				case "R": right();
+					break;
+				case "L": left();
+					break;
+			}
+		});
+	});
+	$("#reset").on("click", function() {
+		markPoint(context, main[0][0]);
+		output();
+	});
+});
